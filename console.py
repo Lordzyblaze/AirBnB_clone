808e7cdaf2b84f94d1ff42412ff9220c4f1cf46b
Adams Adjartey <melawzy421@yahoo.com>
2022-10-30 00:46:56 +0000

    more fix

--- console.py
+++ console.py
@@ -41,8 +41,11 @@ class HBNBCommand(cmd.Cmd):
         cmd.Cmd.do_help(self, args)
 
     def do_create(self, args):
-        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
-        Create a new class instance with given keys/values and print its id."""
+        """
+        Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with given keys/values and print its id.
+
+        """
         my_list = args.split()
         if not my_list:
             print("** class name missing **")
@@ -61,8 +64,11 @@ class HBNBCommand(cmd.Cmd):
 
 
     def do_show(self, args):
-        """Prints the string representation of an instance
-        based on the class name and id"""
+        """
+        Prints the string representation of an instance
+        based on the class name and id
+
+        """
         list_str = args.split()
         if not list_str:
             print("** class name missing **")
@@ -79,8 +85,11 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
 
     def do_destroy(self, args):
-        """Deletes an instance based on the class name and id
-        (save the change into the JSON file)"""
+        """
+        Deletes an instance based on the class name and id
+        (save the change into the JSON file)
+
+        """
         list_str = args.split()
         if not list_str:
             print("** class name missing **")
@@ -98,8 +107,11 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
 
     def do_all(self, args):
-        """Prints all string representation of all instances
-        based or not on the class name"""
+        """
+        Prints all string representation of all instances
+        based or not on the class name
+
+        """
         list_str = args.split()
         if not args or list_str[0] in HBNBCommand.allowed_obj:
             str_list = []
@@ -111,8 +123,11 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
 
     def do_update(self, args):
-        """Updates an instance based on the class name and id by adding
-        or updating attribute (save the change into the JSON file)"""
+        """
+        Updates an instance based on the class name and id by adding
+        or updating attribute (save the change into the JSON file)
+
+        """
         list_str = args.split()
         if not list_str:
             print("** class name missing **")
